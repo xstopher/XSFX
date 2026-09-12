a6fa75d4a5f4e9947ee93fc53d805bd831b72f39
xstopher <jayden.odhiambo8@gmail.com>
2026-09-12 23:12:08 +0300

    Add XSFX app launch animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,17 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <div className="app-launch" aria-hidden="true">
+        <div className="app-launch-mark">
+          <div className="app-launch-square app-launch-square-outer" />
+          <div className="app-launch-square app-launch-square-mid" />
+          <div className="app-launch-square app-launch-square-inner" />
+        </div>
+        <span className="app-launch-name">XSFX</span>
+      </div>
+    </>
+  );
 }
